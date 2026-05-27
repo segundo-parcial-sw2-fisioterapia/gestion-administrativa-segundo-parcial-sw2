b28a04854cd98b506297750056d6890839abaadc
Choque Quispe Wilder <wilderc.q.16@gmail.com>
2026-05-27 09:40:40 -0400

    first commit

---
+++ src/main/java/com/fisioterapia/gestion_administrativa_segundo_parcial_sw2/GestionAdministrativaSegundoParcialSw2Application.java
@@ -0,0 +1,13 @@
+package com.fisioterapia.gestion_administrativa_segundo_parcial_sw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAdministrativaSegundoParcialSw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAdministrativaSegundoParcialSw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fisioterapia/gestion_administrativa_segundo_parcial_sw2/GestionAdministrativaSegundoParcialSw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisioterapia.gestion_administrativa_segundo_parcial_sw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAdministrativaSegundoParcialSw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
